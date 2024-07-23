82351c94920567093ac3a3868559b6ef33f8b909
E-Abeni <tamiratabenezer1@gmail.com>
2024-07-24 00:32:47 +0300

    Revert "updated homepage layout"
    This reverts commit ce1a688f2082a8e3b76f6b9e3c8de896dd322b85.

--- app/page.tsx
+++ app/page.tsx
@@ -28,38 +28,35 @@ function ParentDiv(){
 
 	return(
 		<main className="flex flex-row h-full">
-		<div className="w-max flex flex-col p-7 bg-blue-900 content-center justify-center w-4/12"> 
+		<div className="w-1/2 flex flex-col p-7 bg-blue-900 content-center justify-center"> 
 			<h1 className = {clsx(
-				`${cinzel.className} border-solid border-2 rounded-full text-5xl  h-25 italic font-mono text-center`, 
+				"border-solid border-2 rounded-full text-5xl  h-25 self-center justify-self-start italic font-mono text-center", 
 				{
-					"bg-gray-300 p-5 text-black w-auto": status === 0,
-					"bg-white p-5 text-black w-auto" : status !== 0
+					"bg-black p-11 text-white w-auto": status !== 0,
+					"bg-white p-11 text-black w-auto" : status === 0
 				})} onMouseEnter={hovered}
 					onMouseLeave={unhovered}> I-Follow </h1>
 
-			<ul className = "flex flex-grow flex-col justify-center h-1/4 w-auto">
+			<ul className = "flex flex-grow flex-col justify-center h-1/4">
 				{content.map((element) => <li className={`${cinzel.className} font-mono text-center text-2xl m-5 text-white`}> {element} </li>)}
 			</ul> 
+			<div className = "h-1/4"></div>
+			
+		</div>
+		
+		<div className="w-1/2 gap-2 flex flex-col p-2 items-center justify-between bg-gray-200">
 			<Image 
 				src = "/ethi_health.jpg"
 				alt = "image"
 				width = "1700"
 				height = "700"
-				className = "block w-auto rounded-2xl shadow-2xl my-4"
+				className = "block w-9/12 rounded-2xl shadow-2xl"
 			/>
-			
-			
-		</div>
-		
-		<div className="w-9/12 flex flex-col p-2 items-center justify-center bg-gray-200">
-			
-			<div className = "bg-gray-300 flex flex-col p-10 rounded-3xl w-1/2 h-4/6 border-solid border-2 border-black">
-				<label className="p-1 mx-4 my-1"> User Name </label>
-				<input className="p-1 my-1 border-solid border-black border-2 rounded-md mx-4 " type = "text" placeholder="example@example.com"></input>
-				<label className="p-1 mx-4 my-1 space-y-40"> Password </label>
-				<input className="p-1 my-1 border-solid border-black border-2 rounded-md mx-4 " type = "password" placeholder="***********"></input>
-				<Link className = "my-4 self-center bg-blue-900 rounded-md m-4 w-3/12 text-white p-2 text-md text-center" href = "Patient"><button className="" type = "button">Login</button></Link>
-				<div className="my-1 flex flex-row w-9/12 justify-around self-center"><a href="">Forget password?</a> <a href="">Sign Up</a></div>
+			<div className = "bg-gray-300 flex flex-col p-10 rounded-3xl w-9/12 h-auto border-solid border-2 border-black">
+				<input className="p-1 border-solid border-black border-2 rounded-md m-4 " type = "text" placeholder="User Name"></input>
+				<input className="p-1 border-solid border-black border-2 rounded-md m-4 " type = "password" placeholder="Password"></input>
+				<Link className = "self-center bg-blue-900 rounded-md m-4 w-3/12 text-white p-2 text-md text-center" href = "Patient"><button className="" type = "button">Login</button></Link>
+				<div className="flex flex-row w-9/12 justify-around self-center"><a href="">Forget password?</a> <a href="">Sign Up</a></div>
 			</div>
 		</div>
 		</main>
