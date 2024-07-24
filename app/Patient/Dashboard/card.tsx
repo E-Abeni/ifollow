689e0390279041cd39308b57dcff2c9e5ff631d2
E-Abeni <tamiratabenezer1@gmail.com>
2024-07-24 13:25:16 +0300

    corrected deploy error caused by type mismatch

--- app/Patient/Dashboard/card.tsx
+++ app/Patient/Dashboard/card.tsx
@@ -1,4 +1,7 @@
-export default function Card({title, Display}){
+export default function Card({title, Display} : {
+	title: String,
+	Display: React.ReactNode
+}){
 	return (
 		<div className="flex flex-col h-fit m-4 mx-10 w-4/12 h-5/12/4 border-solid border-2 rounded-2xl border-black p-2 bg-gray-200">
 			<h1 className="bg-gray-200"> {title} </h1>
