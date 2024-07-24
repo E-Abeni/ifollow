8cf7da484071c57ead6d8ccfc1d4e0b88d321d01
E-Abeni <tamiratabenezer1@gmail.com>
2024-07-24 13:37:30 +0300

    corrected deploy error caused by type mismatch

--- app/Patient/page.tsx
+++ app/Patient/page.tsx
@@ -1,20 +1,20 @@
 import Card from './Dashboard/card';
-import Table from './Dashboard/page';
+
 
 
 export default function page(){
 	return(
 	<div className="flex flex-row flex-wrap h-fit">
 		
-		<Card title="My Doctors" Display = {Table}/>
-		<Card title="TO-DO" Display = {Table}/>
-		<Card title="New Messages" Display = {Table}/>
-		<Card title="What to Read" Display = {Table}/>
+		<Card title="My Doctors"/>
+		<Card title="TO-DO" />
+		<Card title="New Messages"/>
+		<Card title="What to Read" />
 		
-		<Card title="My Doctors" Display = {Table}/>
-		<Card title="TO-DO" Display = {Table}/>
-		<Card title="New Messages" Display = {Table}/>
-		<Card title="What to Read" Display = {Table}/>
+		<Card title="My Doctors"/>
+		<Card title="TO-DO" />
+		<Card title="New Messages"/>
+		<Card title="What to Read"/>
 	
 	</div>
 	)
